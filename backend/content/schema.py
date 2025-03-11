0050730f0b6bf9d5ed9ab7fc83b68912dc65134a
Sleeeee <jstasjustin@gmail.com>
2025-03-11 11:51:44 +0100

    DBOX #17 Add graphene mutations CreateView and CreateVote

--- backend/content/schema.py
+++ backend/content/schema.py
@@ -2,7 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from django.contrib.auth import get_user_model
 from django.conf import settings
-from .models import Audio, Publication, Comment, Tag
+from .models import Audio, Publication, View, Vote, Comment, Tag
 from users.schema import UserType
 from graphene_file_upload.scalars import Upload
 from graphql import GraphQLError
@@ -24,6 +24,16 @@ class PublicationType(DjangoObjectType):
         if root.cover:
             return root.cover.url
         return None
+
+class ViewType(DjangoObjectType):
+    class Meta:
+        model = View
+        fields = ("id", "publication", "user")
+
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
+        fields = ("id", "publication", "user", "type")
 
 class CommentType(DjangoObjectType):
     class Meta:
@@ -82,7 +92,44 @@ class CreatePublication(graphene.Mutation):
         publication.save()
 
         return CreatePublication(publication=publication)
-        
+
+class CreateView(graphene.Mutation):
+    class Arguments:
+        publication_id = graphene.Int(required=True)
+
+    view_count = graphene.Int()
+
+    def mutate(root, info, publication_id):
+        user = info.context.user
+        if not user.is_authenticated:
+            return CreateView(view_count=None)
+        if View.objects.filter(publication_id=publication_id, user=user).exists():
+            return CreateView(view_count=None)
+
+        publication = Publication.objects.get(id=publication_id)
+        View.objects.create(publication=publication, user=user)
+        publication.refresh_from_db()
+        return CreateView(view_count=publication.view_count)
+
+class CreateVote(graphene.Mutation):
+    class Arguments:
+        publication_id = graphene.Int(required=True)
+        type = graphene.Int(required=True)
+
+    vote_count = graphene.Int()
+
+    def mutate(root, info, publication_id, type):
+        user = info.context.user
+        if not user.is_authenticated:
+            return CreateVote(vote_count=None)
+        if Vote.objects.filter(publication_id=publication_id, user=user).exists():
+            return CreateVote(vote_count=None)
+
+        publication = Publication.objects.get(id=publication_id)
+        Vote.objects.create(publication=publication, user=user, type=type)
+        publication.refresh_from_db()
+        return CreateVote(vote_count=publication.vote_count)
+
 class CreateComment(graphene.Mutation):
     class Arguments:
         publication = graphene.Int(required=True)  
@@ -94,20 +141,17 @@ class CreateComment(graphene.Mutation):
     def mutate(self, info, publication, text, parent=None):
         if not info.context.user.is_authenticated:
             raise GraphQLError("You must be logged in to comment.")
-
-        
         try:
             publication_instance = Publication.objects.get(id=publication)
         except Publication.DoesNotExist:
             raise GraphQLError("Publication not found.")
-
-       
         parent_comment = None
         if parent:
             try:
                 parent_comment = Comment.objects.get(id=parent)
             except Comment.DoesNotExist:
                 raise GraphQLError("Parent comment not found.")
+
         author = info.context.user
         comment = Comment(publication = publication_instance, parent=parent_comment, text=text, author=author)
         comment.save()
@@ -115,6 +159,8 @@ class CreateComment(graphene.Mutation):
     
 class Mutation(graphene.ObjectType):
     create_publication = CreatePublication.Field()
+    create_view = CreateView.Field()
+    create_vote = CreateVote.Field()
     create_comment = CreateComment.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
