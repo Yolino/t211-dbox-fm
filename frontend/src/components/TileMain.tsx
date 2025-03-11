a6c41867fe976f25a317e0adc19fbae2871a8b59
Sleeeee <jstasjustin@gmail.com>
2025-03-11 14:04:20 +0100

    DBOX #17 Implement base TileExpanded for publication details, call mutation on tile click

--- frontend/src/components/TileMain.tsx
+++ frontend/src/components/TileMain.tsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 import TileGroup from "./TileGroup.tsx";
 import TileExpanded from "./TileExpanded.tsx";
+import { useMutation } from "@apollo/client";
+import CREATE_VIEW_MUTATION from "../graphql/createViewMutation.ts";
 
 const TileMain = ({ onPlayAudio }) => {
   const SORT_TYPES = ["-created_at", "-vote_count"];
   const [expandedTile, setExpandedTile] = useState(null);
+
+  const [incrementViewCount] = useMutation(CREATE_VIEW_MUTATION);
+
+  const handlePlayAudio = (tileId) => {
+    onPlayAudio(tileId);
+  };
+
+  const handleExpandTile = (tileId, groupId) => {
+    setExpandedTile(
+      expandedTile?.tileId === tileId && expandedTile.groupId === groupId ? null : { tileId, groupId }
+    );
+    if ((!expandedTile?.tileId) || (expandedTile?.tileId === +tileId)) incrementViewCount({variables : {publicationId: +tileId} }).catch(console.error);
+  };
 
   return (
     <div>
@@ -12,12 +27,8 @@ const TileMain = ({ onPlayAudio }) => {
         <React.Fragment key={orderBy}>
           <TileGroup
             orderBy={orderBy}
-            onPlayAudio={onPlayAudio}
-            onTileClick={(tileId, groupId) =>
-              setExpandedTile(
-                expandedTile?.tileId === tileId && expandedTile.groupId === groupId ? null : { tileId, groupId }
-              )
-            }
+            onPlayAudio={handlePlayAudio}
+            onTileClick={handleExpandTile}
           />
           {expandedTile?.groupId === orderBy && <TileExpanded tileId={expandedTile.tileId} />}
         </React.Fragment>
