77bbc979f40833f6ecc788b6fe68587c95cf8aa7
Sleeeee <jstasjustin@gmail.com>
2025-03-13 14:33:13 +0100

    Relocate Django secret key to the env file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret! Place in .env.backend
-SECRET_KEY = 'django-insecure-mw(t8-9a+t2%bbs%!=@fe)c4(lloxfu8g!*!*nk46m34-08*s^'
+SECRET_KEY = os.getenv(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
