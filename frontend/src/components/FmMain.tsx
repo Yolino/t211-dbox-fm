0a8f26860ce61b99e24ef29de3fe880a89506a2a
Sleeeee <jstasjustin@gmail.com>
2025-03-17 16:43:50 +0100

    DBOX #12 Render basic schedule on the frontend

--- frontend/src/components/FmMain.tsx
+++ frontend/src/components/FmMain.tsx
@@ -5,16 +5,31 @@ import SCHEDULE_QUERY from "../graphql/scheduleQuery.ts";
 
 const FmMain = () => {
   const { privileges } = usePrivileges();
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
   const { loading, error, data } = useQuery(SCHEDULE_QUERY, {
     variables: { date },
   });
-  console.log(data);
+  const schedule = data?.schedule;
+
+  const incrementDate = (increment) => {
+    const currentDate = new Date(date);
+    currentDate.setDate(currentDate.getDate() + increment);
+    setDate(currentDate.toISOString().split('T')[0]);
+  };
 
   return (
     <div>
       <p>Dbox FM</p>
       {privileges?.isModerator && <p>You are a moderator</p>}
+
+      <button onClick={() => { incrementDate(-1) }}>Previous</button>
+      <input type="date" value={date} onChange={(e) => { setDate(e.target.value) }} />
+      <button onClick={() => { incrementDate(1) }}>Next</button>
+
+      {schedule?.length === 0 && <p>Nothing is scheduled for that date</p>}
+      {schedule?.map((s) => (
+        <p>{s.time} - {s.publication.title}</p>
+      ))}
     </div>
   );
 };
