b459815449fb1d7b94f4290b34b43c92d0073f2d
Sleeeee <jstasjustin@gmail.com>
2025-03-20 08:15:45 +0100

    DBOX #12 Add DeleteScheduling mutation

--- backend/live/schema.py
+++ backend/live/schema.py
@@ -29,7 +29,7 @@ class CreateScheduling(graphene.Mutation):
     def mutate(root, info, publication_id, time):
         user = info.context.user
         if not user.is_authenticated:
-            raise GraphQLError("You must be logged in to add to the schedule")
+            raise GraphQLError("You must be logged in as a privileged user to add to the schedule")
         if not user.has_perm("live.add_scheduling"):
             raise GraphQLError("You do not have the required permissions to add to the schedule")
         # WARNING : a timedelta of one day should be added in production : if time < (datetime.now() + timedelta(days=1))
@@ -43,6 +43,25 @@ class CreateScheduling(graphene.Mutation):
         scheduling.save()
         return CreateScheduling(scheduling=scheduling)
 
+class DeleteScheduling(graphene.Mutation):
+    class Arguments:
+        scheduling_id = graphene.Int(required=True)
+
+    success = graphene.Boolean()
+
+    def mutate(root, info, scheduling_id):
+        user = info.context.user
+        if not user.is_authenticated:
+            raise GraphQLError("You must be logged in as a privileged user to delete from the schedule")
+        if not user.has_perm("live.delete_scheduling"):
+            raise GraphQLError("You do not have the required permissions to delete from the schedule")
+        try:
+            scheduling = Scheduling.objects.get(id=scheduling_id)
+        except Scheduling.DoesNotExist:
+            raise GraphQLError("This Scheduling does not exist")
+        scheduling.delete()
+        return DeleteScheduling(success=True)
+
 class Mutation(graphene.ObjectType):
     create_scheduling = CreateScheduling.Field()
-
+    delete_scheduling = DeleteScheduling.Field()
