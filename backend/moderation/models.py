47de31aa91aeb3118b5501184d4cffbdc480372d
Sleeeee <jstasjustin@gmail.com>
2025-04-11 11:49:21 +0200

    DBOX #19 Add basic report querying

--- backend/moderation/models.py
+++ backend/moderation/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth.models import User
 from content.models import Publication, Comment
 
 class ReportUser(models.Model):
-    reporter = models.ForeignKey(User, related_name="reporter", on_delete=models.PROTECT)
-    reported_user = models.ForeignKey(User, related_name="reported_user", on_delete=models.PROTECT)
+    reporter = models.ForeignKey(User, related_name="reports_created", on_delete=models.PROTECT)
+    reported_user = models.ForeignKey(User, related_name="reports_received", on_delete=models.PROTECT)
     is_reviewed = models.BooleanField(default=False)
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=["reporter", "reported_user"], name="unique_report_user"),
         ]
+    def __str__(self):
+        return f"{self.reported_user} reported by {self.reporter}"
 
 class ReportPublication(models.Model):
     reporter = models.ForeignKey(User, on_delete=models.PROTECT)
@@ -20,6 +22,9 @@ class ReportPublication(models.Model):
             models.UniqueConstraint(fields=["reporter", "reported_publication"], name="unique_report_publication"),
         ]
 
+    def __str__(self):
+        return f"{self.reported_publication} reported by {self.reporter}"
+
 class ReportComment(models.Model):
     reporter = models.ForeignKey(User, on_delete=models.PROTECT)
     reported_comment = models.ForeignKey(Comment, on_delete=models.PROTECT)
@@ -28,4 +33,7 @@ class ReportComment(models.Model):
         constraints = [
             models.UniqueConstraint(fields=["reporter", "reported_comment"], name="unique_report_comment"),
         ]
+
+    def __str__(self):
+        return f"{self.reported_comment} reported by {self.reporter}"
 
--- backend/moderation/schema.py
+++ backend/moderation/schema.py
@@ -1,5 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
+from django.db import models
+from django.contrib.auth.models import User
 from .models import ReportUser, ReportPublication, ReportComment
 from content.models import Publication, Comment
 
@@ -7,6 +10,15 @@ class ReportUserType(DjangoObjectType):
     class Meta:
         model = ReportUser
         fields = ("id", "reporter", "reported_user", "is_reviewed")
+
+class ReportedUserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ("id", "username", "is_active")
+
+    report_count = graphene.Int()
+    def resolve_report_count(root, info):
+        return root.report_count
 
 class ReportPublicationType(DjangoObjectType):
     class Meta:
@@ -17,16 +29,42 @@ class ReportedPublicationType(DjangoObjectType):
     class Meta:
         model = Publication
         fields = ("id", "title", "author", "cover", "description", "is_banned")
-    
+
     report_count = graphene.Int()
     def resolve_report_count(root, info):
-        pass
+        return root.report_count
 
 class ReportCommentType(DjangoObjectType):
     class Meta:
         model = ReportComment
         fields = ("id", "reporter", "reported_comment", "is_reviewed")
 
+class ReportedCommentType(DjangoObjectType):
+    class Meta:
+        model = Comment
+        fields = ("id", "publication", "author", "text", "is_banned")
+
+    report_count = graphene.Int()
+    def resolve_report_count(root, info):
+        return root.report_count
+
 class Query(graphene.ObjectType):
-    pass
+    reported_users = graphene.List(ReportedUserType)
+    reported_publications = graphene.List(ReportedPublicationType)
+    reported_comments = graphene.List(ReportedCommentType)
+
+    def resolve_reported_users(root, info):
+        if not info.context.user.has_perm("moderation.view_reportuser"):
+            raise GraphQLError("You do not have permission to view User reports")
+        return User.objects.filter(reports_received__is_reviewed=False).annotate(report_count=models.Count("reports_received", filter=models.Q(reports_received__is_reviewed=False))).distinct()
+
+    def resolve_reported_publications(root, info):
+        if not info.context.user.has_perm("moderation.view_reportpublication"):
+            raise GraphQLError("You do not have permission to view Publication reports")
+        return Publication.objects.filter(reportpublication__is_reviewed=False).annotate(report_count=models.Count("reportpublication", filter=models.Q(reportpublication__is_reviewed=False))).distinct()
+
+    def resolve_reported_comments(root, info):
+        if not info.context.user.has_perm("moderation.view_reportcomment"):
+            raise GraphQLError("You do not have permission to view Comment reports")
+        return Comment.objects.filter(reportcomment__is_reviewed=False).annotate(report_count=models.Count("reportcomment", filter=models.Q(reportcomment__is_reviewed=False))).distinct()
 
