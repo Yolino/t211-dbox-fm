0b45d3fe9522e57b6919bb420c8f0164ace07a7c
Sleeeee <jstasjustin@gmail.com>
2025-04-15 13:37:51 +0200

    DBOX #19 Fix reportedContentQuery.ts

--- frontend/src/graphql/reportedContentQuery.ts
+++ frontend/src/graphql/reportedContentQuery.ts
@@ -2,18 +2,20 @@ import { gql } from "@apollo/client";
 
 const REPORTED_CONTENT_QUERY = gql`
   query ReportedContent {
-    users {
-      id
-      username
-    }
-    publications {
-      id
-      title
-      description
-    }
-    comments {
-      id
-      text
+    reportedContent {
+      users {
+        id
+        username
+      }
+      publications {
+        id
+        title
+        description
+      }
+      comments {
+        id
+        text
+      }
     }
   }
 `;
