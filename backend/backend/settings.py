d05761077a25b9eb7d2b554b3bcc56c083c7bc2f
Sleeeee <jstasjustin@gmail.com>
2025-04-16 10:23:10 +0200

    DBOX 0.4.0 Adapt nginx and django configurations for staging email support

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from .secrets import get_secret
 
-WEBSITE_URL = "http://localhost:3000"
+WEBSITE_URL = "http://localhost:80"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
