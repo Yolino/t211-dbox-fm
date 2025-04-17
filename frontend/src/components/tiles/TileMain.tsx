c23a417172a74a7ade9d8e9877461e14614e9af2
Sleeeee <jstasjustin@gmail.com>
2025-04-17 12:22:10 +0200

    DBOX #32 Update TileMail to split the screen and show TileExpanded on the size rather than on the bottom

--- frontend/src/components/tiles/TileMain.tsx
+++ frontend/src/components/tiles/TileMain.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MainBlock from "../MainBlock.tsx";
 import TileGroup from "./TileGroup.tsx";
 import TileExpanded from "./TileExpanded.tsx";
 import { useMutation } from "@apollo/client";
@@ -6,7 +7,7 @@ import CREATE_VIEW_MUTATION from "../../graphql/createViewMutation.ts";
 
 const TileMain = ({ onPlayAudio }) => {
   const SORT_TYPES = ["-created_at", "-vote_count"];
-  const [expandedTile, setExpandedTile] = useState(null);
+  const [expandedTile, setExpandedTile] = useState(1);
   const [error, setError] = useState(null);
   const [incrementViewCount] = useMutation(CREATE_VIEW_MUTATION);
 
@@ -24,30 +25,32 @@ const TileMain = ({ onPlayAudio }) => {
   };
 
   return (
-    <div>
-      {error && <p className="text-red-500">{error}</p>}
-      {SORT_TYPES.map((orderBy) => {
-	let groupTitle="Unexpected"
-
-	if(orderBy==="-created_at") {
-		groupTitle="Recent"
-	} else if (orderBy==="-vote_count") {
-		groupTitle="Most voted"
-		}
-
-	return (
-        <React.Fragment key={orderBy}>
-          <TileGroup
-	    groupTitle={groupTitle}
-            orderBy={orderBy}
-            onPlayAudio={handlePlayAudio}
-            onTileClick={handleExpandTile}
-            onError={handleError}
-          />
-          {expandedTile?.groupId === orderBy && <TileExpanded tileId={expandedTile.tileId} onError={handleError} />}
-        </React.Fragment>
-	);
-      })}
+    <div className="flex gap-10 h-[calc(100vh-250px)] min-h-96">
+      <MainBlock styleClass="w-1/3 h-full overflow-y-auto">
+        {expandedTile && <TileExpanded tileId={expandedTile.tileId} onError={handleError} />}
+      </MainBlock>
+      <MainBlock styleClass="w-2/3 h-full overflow-y-auto">
+        {error && <p className="text-red-500">{error}</p>}
+        {SORT_TYPES.map((orderBy) => {
+	        let groupTitle="Unexpected"
+	        if(orderBy==="-created_at") {
+		        groupTitle="Recent"
+	        } else if (orderBy==="-vote_count") {
+		        groupTitle="Most voted"
+		      }
+	        return (
+            <React.Fragment key={orderBy}>
+              <TileGroup
+	              groupTitle={groupTitle}
+                orderBy={orderBy}
+                onPlayAudio={handlePlayAudio}
+                onTileClick={handleExpandTile}
+                onError={handleError}
+              />
+            </React.Fragment>
+	        );
+        })}
+      </MainBlock>
     </div>
   );
 };
